b61707f817943063cff424d527e51606f2b15316
RingTong8 <129569708+RingTong8@users.noreply.github.com>
2023-11-01 14:34:21 +0800

    Update server.ts

--- app/config/server.ts
+++ app/config/server.ts
@@ -37,8 +37,11 @@ export const getServerSideConfig = () => {
     );
   }
 
+  const apiKeys = (process.env.OPENAI_API_KEY ?? '' ).split(',')
+  const apiKey = apiKeys.at(Math.floor(Math.random() * apikeys.length)) ?? ''
+    
   return {
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey，
     code: process.env.CODE,
     codes: ACCESS_CODES,
     needCode: ACCESS_CODES.size > 0,
